b5f6febd02e4455df33bfeb5ff1d4a70b355a12f
Steven <theepicsnail@gmail.com>
2015-07-25 00:53:45 -0700

    Fixing project.js, speeding up saving and better logging

--- project.js
+++ project.js
@@ -4,22 +4,23 @@ serialize = function(obj) { return JSON.stringify(obj, null, 2); }
 
 module.exports = function(save_path, save_ms) {
   var last_save = "";
-  var save_ms = (save_ms|0) || 5000; // Default to 5s
+  var save_ms = (save_ms|0) || 500; // Default to 5s
   var obj = {};
 
   // Load default.
   try {
     obj = require(save_path);
-    last_save = serializ(obj);
+    console.log("Loaded", save_path);
+    last_save = serialize(obj);
   }catch(err){
-    console.log(err);
+    console.log("Error loading", err, save_path);
   }
 
   setInterval(function() {
     var save = serialize(obj);
     if (save == last_save)
       return;
-
+    console.log("Updating save");
     last_save = save;
     fs.writeFile(save_path, save, function(err) {
       if(err)
